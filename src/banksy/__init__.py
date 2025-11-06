f49da073eb50018c4c8a95119e03ec2e29b07345
Vipul Singhal <vipulsinghal02@gmail.com>
2025-11-06 15:40:39 -0800

    Add pip-installable package infrastructure
    - Add pyproject.toml with proper package metadata and dependencies
    - Add .gitignore for build artifacts and pycache
    - Create __init__.py files for banksy and banksy_utils packages
    - Package version set to 1.2.1 to match branch version

---
+++ src/banksy/__init__.py
@@ -0,0 +1,19 @@
+"""
+BANKSY: Spatial Clustering Algorithm that Unifies Cell-Typing and Tissue Domain Segmentation
+
+Building Aggregates with a Neighborhood Kernel and Spatial Yardstick (BANKSY) is a method
+for clustering spatial transcriptomic data by augmenting the transcriptomic profile of each
+cell with an average of the transcriptomes of its spatial neighbors.
+
+For more details, see: https://github.com/prabhakarlab/Banksy_py
+"""
+
+__version__ = "1.2.1"
+__author__ = "Vipul Singhal, Nigel Chou, Joseph Lee, Yifei Yue"
+
+# Users should import from submodules:
+# from banksy.initialize_banksy import initialize_banksy
+# from banksy.run_banksy import run_banksy_multiparam
+# from banksy.embed_banksy import generate_banksy_matrix
+
+__all__ = ["__version__"]
